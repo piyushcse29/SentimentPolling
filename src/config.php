2fe40374c874d1ac9d4e705d34408fccc63d21cf
Piyush Mittal <m@piyushmittal.com>
2015-03-21 09:32:01 +0530

    secondchange

--- src/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
-define('DATUMBOX_API_KEY', '5cbccfaba6861b906bd60800155d728d');
-
-// Configure your Facebook App and get your credentials from https://developers.facebook.com/
-define('FACEBOOK_APP_ID', '781347131951039');
-define('FACEBOOK_APP_SECRET', '3f0f275fad67ab6e1f6fb70f041bf9b6'); 
